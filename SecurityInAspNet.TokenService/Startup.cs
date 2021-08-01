dd3378d0bf773bfca795bf73e6595f07638136e5
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-01 18:59:23 +0430

    Added identity in startup file

--- SecurityInAspNet.TokenService/Startup.cs
+++ SecurityInAspNet.TokenService/Startup.cs
@@ -16,6 +16,8 @@ namespace SecurityInAspNet.TokenService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +28,17 @@ namespace SecurityInAspNet.TokenService
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseIdentityServer();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
